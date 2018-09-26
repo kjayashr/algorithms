9f72f93c99da0aebceb794f94f206876bc1e9f3a
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-09-25 21:30:02 -0700

    Detecting Cycle in List
    Cycle is found by slow pointer and fats pointer. When the fast pointer and slow pointer meet, they will meet again at the node, where the cycle started.

---
+++ detectCycle.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+       
+        while( slow!=null &&fast!=null && fast.next!=null)
+         {
+            fast =fast.next.next;
+            slow = slow.next;
+            if(fast == slow)
+            {
+                fast = head;
+                while(fast != slow)
+                {
+                    fast = fast.next;
+                    slow = slow.next;
+                    
+                }
+            return fast;    
+            }
+         }
+     return null;
+    }
+}
