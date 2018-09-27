efe5a3e21745483d91f4c489a621ef5de118fd8e
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-09-26 22:44:05 -0700

    This code can be optimized!

--- findIntersectionofLists.java
+++ findIntersectionofLists.java
@@ -5,13 +5,13 @@ public class Solution {
         int lengthB = 0;
         ListNode tempA = headA;
           ListNode tempB = headB;
-      
+      /*This is a repetative function and can be modularized */
         while(tempA!=null)
         {
             tempA = tempA.next;
             lengthA++;
         }
-        
+        /*This is a repetative function and can be modularized */  
          while(tempB!=null)
         {
             tempB = tempB.next;
