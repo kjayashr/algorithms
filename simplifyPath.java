e89ee1310edfd9b1dc7e6d9013a2ec4b0ef301ec
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-10-08 22:27:58 -0700

    Simplify Path
    Directory structure for new file

---
+++ simplifyPath.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String simplifyPath(String path) {
+          Stack<String> stack = new Stack<String>();
+        
+          Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
+          for ( String dir : path.split("/"))
+          {
+              if(dir.equals("..") && !stack.isEmpty())
+              {
+                  stack.pop();
+              }
+              else if(!skip.contains(dir))
+                stack.push(dir);
+          }     
+        String res = "";
+        //System.out.println(stack.pop());
+        while(!stack.isEmpty())
+        {
+            res =  "/"+ stack.pop()+res;
+        }
+       
+        return res.isEmpty() ? "/": res;
+}
+}
