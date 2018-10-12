182f82b5b347e694174c867b6d8a1414008c6fdc
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-10-11 21:42:46 -0600

    Update isBalanced.java
    1. Calculate the depth and doifference at each level./
    2. Depth is calculated recursievely

--- isBalanced.java
+++ isBalanced.java
@@ -1,16 +1,4 @@
-/**
- * Definition for binary tree
- * class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) {
- *      val = x;
- *      left=null;
- *      right=null;
- *     }
- * }
- */
+
 public class Solution {
     public boolean /**
  * Definition for binary tree
