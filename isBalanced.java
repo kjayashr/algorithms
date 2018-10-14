7232ad13933466c48636ac76897c5f985f24d1ac
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-10-13 23:14:46 -0700

    Updated code has multiple recursive calls.
    Commented clean code.

--- isBalanced.java
+++ isBalanced.java
@@ -30,18 +30,21 @@ return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 
     
 }(TreeNode A) {
+    
+    //base case
       if(A == null)
       {
           return true;
       }
       
-        
+        //calculate length
             int leftH  = maxDepth(A.left);
             int rightH = maxDepth(A.right);
-            
+           //difference should be less than 1 and recursievly call balanced functions.
         if((Math.abs( leftH - rightH )  <= 1) && (isBalanced(A.left)) &&  (isBalanced(A.right))) {
             return true;
         }
+
         else
         {
             return false;
@@ -52,8 +55,8 @@ return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
     
     
-
-        public int maxDepth(TreeNode root) {
+//this helper will be called by every helper function.
+  public int maxDepth(TreeNode root) {
 if (root == null) return 0;
 return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
