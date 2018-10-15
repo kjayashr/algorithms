b95cb52053b7dce4f77b6f74ba9ebd17a8709480
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-10-14 17:40:21 -0700

    LCA when parent is given

---
+++ withParentInfo.java
@@ -0,0 +1,33 @@
+public static BinaryTree<Integer> LCA ( BinaryTree <Integer > noder, BinaryTree <Integer > nodel) {
+
+int depthr = getDepth(noder), depthl = getDepth(node1);
+
+            if (depthl > depthr) {
+                       BinaryTree <Integer> temp = noder;
+                       noder = node1 ;
+                       nodel = temp;
+                }
+
+int depthDiff = Math.abs (depthr - depthl);
+ while (depthDiff-- > 0) {
+     noder = noder.parent ;
+     }
+
+ while (noder != nodel)
+ { 
+ noder = noder.parent ; 
+ nodel = node1.parent ;
+}
+
+return noder;
+}
+
+
+private static int getDepth(BinaryTree<Integer> node) { 
+int depth = 0;
+while (node. parent != null){
+   ++depth ;
+   node = node. parent;
+  }
+ return depth;
+}
