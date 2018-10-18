8c5c2e6aab593cf02c2708350d2bd15e211e60a5
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-10-17 19:41:25 -0700

    LC32 - Longest valid paranthesis

---
+++ longestValidParentheses.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+                 System.out.println("l1: Pushed "+ i);
+            } else {
+                System.out.println("l2: Popped "+stack.peek()+" when i is  "+ i);
+                stack.pop();
+                
+                if (stack.empty()) {
+                    stack.push(i);
+                    System.out.println("l3: Pushed "+ i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+}
