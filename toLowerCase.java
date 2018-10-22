6c9bd47a8b2eb057bef0ea9efba0a08ae1f58e90
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-10-22 09:09:48 -0700

    Change a String of Upper Case to Lower Case

---
+++ toLowerCase.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String toLowerCase(String str) {
+        char []c = str.toCharArray();
+        for(int i = 0; i< c.length;i++)
+        {
+           if(c[i] >= 'A' && c[i] <= 'Z' )
+           {
+                c[i] = (char) (c[i] - 'A' + 'a');
+           }
+        }
+        return new String(c);
+    }
+}
