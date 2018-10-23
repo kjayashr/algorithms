62d550db009b0f6127f6674e1a650e2f2c1f5605
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-10-23 12:30:02 -0700

    Update validNumber.java

--- validNumber.java
+++ validNumber.java
@@ -1,3 +1,23 @@
+/*Basically the prgoram should match this RE
+
+[-+]?(([0-9]+(.[0-9]*)?)|.[0-9]+)(e[-+]?[0-9]+)? */
+
+
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+" -90e3   " => true
+" 1e" => false
+"e3" => false
+" 6e-1" => true
+" 99e2.5 " => false
+"53.5e93" => true
+" --6 " => false
+"-+3" => false
+"95a54e53" => false
+*/
 class Solution {
   public boolean isNumber(String s) {
     s = s.trim();
@@ -33,3 +53,7 @@ class Solution {
     return numberSeen && numberAfterE;
 }
 }
+
+/*LC 65 */
+
+
