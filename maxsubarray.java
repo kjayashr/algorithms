c13fb52dbe86a87b49a5009f9568529cf9bd3fe4
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-10-25 07:53:51 -0700

    Create maxsubarray.java

---
+++ maxsubarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxsofar  = 0;
+        int currmax = 0;
+        for(int i =0;i<nums.length;i++)
+        {
+            currmax = Math.max(nums[i],currmax+nums[i]);
+            maxsofar = Math.max(currmax,maxsofar);
+        }
+        return maxsofar;
+    }
+}
+/*
+Sum of Maximum subarray
+*/
