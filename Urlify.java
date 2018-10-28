c4b5deca8ab37868d732add8705cc25799d19206
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-10-27 17:22:41 -0700

    Create Urlify.java

---
+++ Urlify.java
@@ -0,0 +1,18 @@
+    public static StringBuffer Url(String s)
+    {
+       StringBuffer sb = new  StringBuffer();
+         //sb.append("");    //char ch = '%';
+      for(int i =0 ; i<s.length();i++)
+      {
+          if((s.charAt(i) ==' '))
+          {
+           sb.append("%20");
+          }
+          else
+          {
+            sb.append(s.charAt(i));   
+          }
+      }
+       
+        return sb;
+    }
