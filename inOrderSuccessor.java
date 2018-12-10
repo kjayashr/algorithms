51d1bc2f045283abeb0fe2b646a7b015f5aba533
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-12-10 13:17:07 -0800

    /*LS:12/10/2019*/

---
+++ inOrderSuccessor.java
@@ -0,0 +1,19 @@
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        
+        if(root == null)
+            return null;
+        
+        if(root.val <= p.val)
+            return inorderSuccessor(root.right, p);
+        else {
+            TreeNode left = inorderSuccessor(root.left,p);
+            return (left != null)?left:root;
+        }
+ 
+    }
+    
+    
+}
+
+/*find next node in an BST*/
