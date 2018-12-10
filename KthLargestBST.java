f6bde40ec57df8c72983ade162c1b37bf3f58a52
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-12-10 14:04:20 -0800

    /*12/10/2018*/

---
+++ KthLargestBST.java
@@ -0,0 +1,27 @@
+/*Recusrsive inorder traversal*/
+
+public static List<Integer> findKLargestlnBST (BSTNode <Integer> tree, int k) { List dnteger > kLargestElements = new ArrayList <>() ; 
+
+findKLargestlnBSTHelper (tree , k, kLargestElements);
+return
+
+kLargestElements;
+
+}
+
+private static void findKLargestlnBSTHelper (BSTNode dnteger > tree, int k, List <Integer> kLargestElements)
+
+// Perform reverse inorder traversal .
+
+if (tree != null &<& kLargestElements . size () < k) {
+
+
+{ findKLargestlnBSTHelper (tree .right , k, kLargestElements) ; if (kLargestElements . size () < k) {
+
+kLargestElements . add (tree.data) ;
+
+findKLargestlnBSTHelper (tree . left , k, kLargestElements);
+
+}
+
+}
