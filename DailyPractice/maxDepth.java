a59d7aba05ef948481704a2b7bab2ccf939ba442
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2018-12-17 07:04:12 -0800

    /*ls:12/17/2018*/

---
+++ DailyPractice/maxDepth.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+      else return Math.max(maxDepth(root.right),maxDepth(root.left))+1;
+    }
+}
