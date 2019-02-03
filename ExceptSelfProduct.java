e787ef1f5d5ff2e071db01babfaeab16aabff7a9
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2019-02-02 20:13:52 -0800

    Update ExceptSelfProduct.java

--- ExceptSelfProduct.java
+++ ExceptSelfProduct.java
@@ -1,16 +1,20 @@
-        public class Solution {
-public int[] productExceptSelf(int[] nums) {
-    int n = nums.length;
-    int[] res = new int[n];
-    res[0] = 1;
-    for (int i = 1; i < n; i++) {
-        res[i] = res[i - 1] * nums[i - 1];
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+     int left = 1;
+        int[] res = new int[nums.length];
+        res[0] = left;
+        for(int i = 1; i<nums.length;i++ )
+        {
+            res[i] = left*nums[i-1];
+            left = res[i];
+        }
+        
+       int right = nums[nums.length-1];
+        for(int i = nums.length-2; i>=0;i-- )
+        {
+            res[i] = right*res[i];
+            right = right*nums[i];
+        }
+        return res;
     }
-    int right = 1;
-    for (int i = n - 1; i >= 0; i--) {
-        res[i] *= right;
-        right *= nums[i];
-    }
-    return res;
 }
-//once from left and once from right
