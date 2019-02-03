c9175b6d3801485a5399399e935b80b127c3760f
Krishnakantah Jayashree Chnadrashekhar <kkanthjc@gmail.com>
2019-02-02 23:16:09 -0800

    In Order Traversal.java

---
+++ InOrderTraversalBST.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+          Stack<TreeNode> stack = new Stack<TreeNode>();
+          List<Integer> res = new ArrayList();
+           TreeNode current = root;
+        while(current!=null || !stack.isEmpty())
+        {
+            while(current!=null)
+            {
+               
+                stack.push(current);
+                 current = current.left;
+            }
+            
+            current = stack.pop();
+            res.add(current.val);
+            current = current.right;
+        }
+        
+        return res;
+    }
+}
